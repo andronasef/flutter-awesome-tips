7c032646cf5f05f037165b2c9df1dfa585232349
Andrew Nasef <androamazing@gmail.com>
2021-08-08 00:51:45 +0200

    Header Icons Only

--- codeweb/src/componets/Header.svelte
+++ codeweb/src/componets/Header.svelte
@@ -5,10 +5,11 @@
     ['Submit Tip', 'https://github.com/andronasef/FlutterAwesomeTips/issues'],
     // ['All Tips', '/#'],
   ]
-  // let iconLinks = [
-  //   ['Submit Tip', '/#'],
-  //   ['All Tips', '/#'],
-  // ]
+  let icons = [
+    ['ic:baseline-add', 'https://github.com/andronasef/FlutterAwesomeTips'],
+    ['mdi:star', 'https://github.com/andronasef/FlutterAwesomeTips'],
+    ['uim:github-alt', 'https://github.com/andronasef/FlutterAwesomeTips'],
+  ]
 </script>
 
 <header class="flex h-[70px] px-5 text-white/85 items-center">
@@ -19,30 +20,17 @@
   />
   <h1 class="font-extrabold text-white text-xl md:text-2xl">Flutter Tips</h1>
   <span class="ml-auto" />
-  <div class="space-x-3 text-lg hidden md:flex">
+  <!-- <div class="space-x-3 text-lg hidden md:flex">
     {#each links as link}
       <a class="transition-all hover:text-white" href={link[1]}>{link[0]}</a>
     {/each}
-  </div>
+  </div> -->
   <div class="space-x-1 mt-2 ml-4 text-2xl">
-    <a
-      href="https://github.com/andronasef/FlutterAwesomeTips"
-      class="transition-all visible md:hidden hover:text-white"
-    >
-      <Icon icon="ic:baseline-add" />
-    </a>
-    <a
-      href="https://github.com/andronasef/FlutterAwesomeTips"
-      class="transition-all hover:text-white"
-    >
-      <Icon icon="mdi:star" />
-    </a>
-    <a
-      href="https://github.com/andronasef/FlutterAwesomeTips"
-      class="transition-all hover:text-white"
-    >
-      <Icon icon="uim:github-alt" />
-    </a>
+    {#each icons as icon}
+      <a href={icon[1]} class="transition-all hover:text-white">
+        <Icon icon={icon[0]} />
+      </a>
+    {/each}
   </div>
 </header>
 
